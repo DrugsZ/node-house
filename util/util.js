a67edafc8d93da023e56408b3df007d5c6af9d51
DrugsZ <1002881508@qq.com>
2018-09-08 11:59:38 +0800

    重新开始

--- util/util.js
+++
@@ -1,5 +0,0 @@
-const headers = require('../config/header.json');
-const fs = require('fs');
-
-
-exports.randomHeader = () => ({'User-Agent':headers[ Math.floor( Math.random() * headers.length) ]})
