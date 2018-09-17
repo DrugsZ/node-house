d3a8c35007019738d441f0f016312da3b3f6dbad
DrugsZ <1002881508@qq.com>
2018-09-17 17:57:38 +0800

    获取对应城市地址

---
+++ util/getCity.js
@@ -0,0 +1,29 @@
+const {independentCityList,cityList } = require('./citys');
+
+const citys = [];
+
+for ( let key in independentCityList) {
+  let cityInfo = Object.create(null);
+  cityInfo.address = `//${independentCityList[key].split('|')[0]}.58.com/`;
+  cityInfo.name = key;
+  citys.push(cityInfo);
+}
+
+for ( let subCitys in cityList) {
+  for( let city in cityList[subCitys] ) {
+    let cityInfo = Object.create(null);
+    cityInfo.address = `//${cityList[subCitys][city].split('|')[0]}.58.com/`;
+    cityInfo.name = city;
+    // citys.push(cityInfo);
+  }
+}
+/**
+ * 通过城市名,获取地址
+ * @param {string} cityName 要获取地址的城市名
+ * @returns {string} cityName对应的地址
+ */
+let getUrlByName = (cityName) => {
+  return citys[0].address;
+};
+
+exports.getUrlByName = getUrlByName;
