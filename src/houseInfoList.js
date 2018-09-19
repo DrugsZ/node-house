96d7712c2eb26149eb6f97c9fef3ef5d3172296d
DrugsZ <1002881508@qq.com>
2018-09-19 17:17:37 +0800

    回家做

--- src/houseInfoList.js
+++ src/houseInfoList.js
@@ -9,7 +9,7 @@ const pageIndex = 1;
 
 const tranUrls = [];
 
- /**
+/**
   * fs写入二次封装
   * @param {string} name-目标文件名;
   * @param {*} data-将要写入的数据;
@@ -17,27 +17,17 @@ const tranUrls = [];
   */
 let saves = (name,data,callback) => {
   fs.writeFile(name,data,(err) => {
-    callback.call(null,err)
+    callback.call(null,err);
   });
-};``
+};
 
 /**
  * 获取指定网页内容
  * 
  * @param {string} url - 要获取html的网址；
- * @param {getDetailList_callback} callback - 对body的处理回调；
  */
-let getDetailList = (url,callback) => {
+let getDetailList =  async(url) => {
   request (`http:${cityUrl}pn${pageIndex}/`,(req,response,body) => {
     
   });
-};
-
-/**
- * @function getDetailList_callback
- * @param {object} body - 响应体
- * @return undefined
- */
-let callback = (body) => {
-
 };
