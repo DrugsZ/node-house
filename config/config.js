5590a1fd269a24bacab4c2099f247ff64cf86e03
DrugsZ <1002881508@qq.com>
2018-10-24 23:16:46 +0800

    feat(config/config):add config file

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const MYSQL_PASS_WORD = 'LJT..1995'; //mysql 密码
+const MYSQL_USER_NAME = 'root'; //mysql 账号名
+
+exports.MYSQL_PASS_WORD = MYSQL_PASS_WORD;
+exports.MYSQL_USER_NAME = MYSQL_USER_NAME;
+
