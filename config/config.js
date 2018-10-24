1d4fcfb112ebba51c6dda0908cff2a71afb74996
DrugsZ <1002881508@qq.com>
2018-10-24 23:18:41 +0800

    feat(config/config):add mysql config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,14 @@
 const MYSQL_PASS_WORD = 'LJT..1995'; //mysql 密码
 const MYSQL_USER_NAME = 'root'; //mysql 账号名
+const MYSQL_DATA_BASE = 'house_info';
+const MYSQL_PORT = '3306';
+const MYSQL_HOST = 'localhost';
 
-exports.MYSQL_PASS_WORD = MYSQL_PASS_WORD;
-exports.MYSQL_USER_NAME = MYSQL_USER_NAME;
+module.exports = {
+  MYSQL_PASS_WORD,
+  MYSQL_USER_NAME,
+  MYSQL_DATA_BASE,
+  MYSQL_PORT,
+  MYSQL_HOST
+};
 
